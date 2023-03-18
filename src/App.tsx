83449ec90f389c1dc70d6df8b879d788e4cd519a
Nikolai Ochirov <dubdubman@ya.ru>
2023-03-18 21:40:49 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./styles.css"
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [date, setDate] = useState(new Date());
+
+  function refresh() {
+    setDate(new Date());
+  }
+
+  useEffect(() => {
+    let timerId = window.setTimeout(refresh, 1000);
+
+    return () => clearInterval(timerId);
+  }, [date]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <p className="timer">{date.toLocaleTimeString()}</p>
+    );
 }
-
-export default App;
